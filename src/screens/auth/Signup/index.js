44c8ba76dc879d1a48afe98d86758fc9b55b2b67
“partelsandra” <sandra.partel.001@voco.ee>
2023-10-04 14:12:34 +0300

    checkbox commit

--- src/screens/auth/Signup/index.js
+++ src/screens/auth/Signup/index.js
@@ -1,15 +1,24 @@
-import { View } from "react-native"
+import React, {useState} from "react";
+import { View, Text } from "react-native"
 import AuthHeader from "../../../components/AuthHeader";
 import { styles } from "./styles";
 import Input from "../../../components/Input";
+import Checkbox from "../../../components/Checkbox";
 
 const Signup = () => {
+    const [checked, setChecked] = useState(false)
+
     return (
         <View style={styles.container}>
             <AuthHeader title="Sign Up"/>
             <Input label="Name" placeholder="John Doe"/>
             <Input label="Email" placeholder="example@gmail.com"/>
             <Input isPassword label="Password" placeholder="******"/>
+
+            <View style={styles.agreeRow}>
+                <Checkbox checked={checked} onCheck={setChecked}/>
+                <Text style={styles.agreeText}>I agree with <Text style={styles.agreeTextBold}>Terms</Text> & <Text style={styles.agreeTextBold}>Privacy</Text></Text>
+            </View>
         </View>
     )
 }
--- src/screens/auth/Signup/styles.js
+++ src/screens/auth/Signup/styles.js
@@ -1,8 +1,45 @@
 import { StyleSheet } from "react-native";
 
+import { colors } from "../../../utils/colors";
+
 export const styles = StyleSheet.create({
-  container: {
+    container: {
+        paddingHorizontal: 20,
+        width: '100%'
+    },
+    inputContainer: {
+        borderColor: colors.grey,
+        borderRadius: 14,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    input: {
+        borderRadius: 14,
+        paddingHorizontal: 16,
+        paddingVertical: 10,
+        flex: 1
+    },
+    label: {
+        marginBottom: 8,
+        color: colors.blue,
+        fontWeight: '500'
+    },
+    agreeRow: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingHorizontal: 10
+    },
+    agreeText: {
+        color: colors.blue,
+        marginHorizontal: 14
+    },
+    agreeTextBold: {
+        color: colors.blue,
+        marginHorizontal: 14,
+        fontWeight: 'bold'
 
-  },
-
-});
+    },
+    button: {
+        marginVertical: 20
+    }
+})
